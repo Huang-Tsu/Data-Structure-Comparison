45be61673d9d770aa5a927c2e1dbeb0c9bf1ae4d
Huang-Tsu <huang0966714@gmail.com>
2021-05-17 17:14:34 +0800

    Completed makeing random number program.
    獲取亂數的方法是，將rand()得到的值除以(2147483647+1.0),再乘上我想要的範圍。
    	提醒:RAND_MAX 加的是"1.0" 不是"1", 因為要讓他變成浮點數
    	不使用%的原因是，%所取的機率調低的數會集中在最後
    	用這個方法的話會比較分散
    
    產生"插入的資料" 跟產生"查詢的資料" 的方法不同
    	1.為了確保"插入的資料"不會重複，用的是洗牌法:
    	    宣告一個陣列，每個陣列的值就等於他的索引值
    	    接著從陣列最後一格開始，每次都隨機獲得在此格之前的任一格，將兩格交換，直到第二格(剩下兩個元素)
        	2.直接用前面說的獲得亂數的方法來得到亂數。

--- make_rand.c
+++ make_rand.c
@@ -1,52 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-enum{
-	INSERT = 1, QUERY = 2
-};
+#define RAND_RANGE ((int)1e6)
 
-int g_mode;
+char *file_for_insert[] = {
+	"",
+	"./test_data/1e4_insert",
+	"./test_data/1e5_insert",
+	"./test_data/1e6_insert"
+};
+char *file_for_query[] = {
+	"",
+	"./test_data/1e3_query",
+	"./test_data/1e4_query",
+	"./test_data/1e5_query"
+};
+void swap(int *array, int idx1, int idx2){
+	static int temp;
+	temp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = temp;
+}
+void initialize_base(int *data, int len){
+	int i;
+	for(i=0; i<len; i++){
+		data[i] = i;
+	}
+}
+void shuffle_base(int *data, int len){
+	int i;
+	int idx;
+	for(i=len-1; i>0; i--){
+		idx = (int)(rand() / (RAND_MAX+1.0) * i);	
+		swap(data, i, idx);
+	}
+
+}
+void print_rand(int *data, int len, char *file_name){
+	FILE *fp;
+	int total = 0;
+
+	fp = fopen(file_name, "w");
+		if(!fp){
+			printf("open %s failed!\n", file_name);
+			exit(1);
+		}
+
+	while(total < len){
+		fprintf(fp, "%d\n", data[RAND_RANGE - 1 - total]);
+		total ++;
+	}
+	fclose(fp);
+}
+void make_rand_for_insert(int *base){
+	int len;
+	initialize_base(base, RAND_RANGE);
+	shuffle_base(base, RAND_RANGE);
+
+	len = (int)1e4;
+	for(int i=1; i<=3; i++){
+		print_rand(base, len, file_for_insert[i]);
+		len *= 10;
+	}
+
+}
+void make_rand_for_query(){
+	FILE *fp;
+	int random_num;
+	int rand_cnt;
+	int len = (int)1e3;
+	for(int i=1; i<=3; i++){
+		rand_cnt = 0;
+
+		fp = fopen(file_for_query[i], "w");	
+			if(!fp){
+				printf("open %s failed!\n", file_for_query[i]);
+				exit(1);
+			}
+
+		while(rand_cnt < len){
+			random_num = (int)(rand() / (RAND_MAX+1.0) * RAND_RANGE);
+			fprintf(fp, "%d\n", random_num);
+			rand_cnt ++;
+		}
+		fclose(fp);
+		
+		len *= 10;
+	}
+}
+
 
 int main(){
-	int total_data;
-	int data_cnt = 0;
-	char mode_input[2];
-	int i=0;
-	int output[(int)1e6] = {0};
+	int shuffle_base[RAND_RANGE];
 
-	puts("Hi welcome to makeing-random-data!");
-	puts("How you gonna use this data?");
-	puts("\t(\"i\" For insert");
-	puts("\t(\"q\" For query");
-	printf("Your answer:");
-
-	scanf("%1s", mode_input);
-	switch(*mode_input){
-		case 'i':
-			g_mode = INSERT;
-			break;
-		case 'q':
-			g_mode = QUERY;
-			break;
-		default:
-			puts("Wrong option");
-			exit(1);
-			break;
-	}
-	puts("Option received.");
-	srand(g_mode);
-	puts("");
-
-	puts("How many do you want?");
-	printf("Your answer:");
-	scanf("%d", &total_data);
-	puts("Option received.");
-	puts("");
-	puts("Start making random data !");
-
-	while(i++<total_data){
-		printf("%d\n", rand());
-	}
-
+	system("mkdir -p test_data");
+	//making data for insert
+	make_rand_for_insert(shuffle_base);
+	make_rand_for_query();
+	//making rand for query
 	return 0;
 }
