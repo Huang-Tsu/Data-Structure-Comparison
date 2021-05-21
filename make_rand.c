e69520754e70994ddef48663642235a95c9f7892
Huang-Tsu <huang0966714@gmail.com>
2021-05-21 20:03:33 +0800

    Change the random number range
    把亂數的範圍變的大一點(1e7)
     這樣hash 才有事可以做
    
     make rand 的方式也有點改變
     改成先洗兩次rand base
     然後取出要insert的100萬筆資料
    
     然後再對這100萬筆資料洗兩次
     再從這100萬筆資料中取要query的十萬筆

--- make_rand.c
+++ make_rand.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define RAND_RANGE ((int)1e6)
-#define START_VALUE_FOR_INSERT ((int)1e4)
-#define START_VALUE_FOR_QUERY ((int)1e3)
+#define RAND_RANGE ((int)1e7)
+#define INSERT_DATA_LEN ((int)1e6)
+#define QUERY_DATA_LEN ((int)1e5)
 
-char *file_for_insert[] = {
-	"",
-	"./test_data/1e4_insert",
-	"./test_data/1e5_insert",
-	"./test_data/1e6_insert"
-};
-char *file_for_query[] = {
-	"",
-	"./test_data/1e3_query",
-	"./test_data/1e4_query",
-	"./test_data/1e5_query"
-};
-char **file_name[] = {
-	file_for_insert,
-	file_for_query
-};
-void swap(int *array, int idx1, int idx2){
+void Swap(int *array, int idx1, int idx2){
 	static int temp;
 	temp = array[idx1];
 	array[idx1] = array[idx2];
 	array[idx2] = temp;
 }
-void initialize_base(int *data, int len){
+void InitializeBase(int *data, int len){
 	int i;
 	for(i=0; i<len; i++){
 		data[i] = i;
 	}
 }
-void shuffle_base(int *data, int len){
+void ShuffleBase(int *data, int len){
 	int i;
 	int idx;
 	for(i=len-1; i>0; i--){
 		idx = (int)(rand() / (RAND_MAX+1.0) * i);	
-		swap(data, i, idx);
+		Swap(data, i, idx);
 	}
 
 }
-void print_rand(int *data, int len, char *file_name){
+void PrintRand(int *data, int len, char *file_name){
 	FILE *fp;
 	int total = 0;
 
@@ -53,37 +37,43 @@ void print_rand(int *data, int len, char *file_name){
 		}
 
 	while(total < len){
-		fprintf(fp, "%d\n", data[RAND_RANGE - 1 - total]);
+		fprintf(fp, "%d\n", data[len - 1 - total]);
 		total ++;
 	}
 	fclose(fp);
 }
-void make_rand(int *base){
-	int i, k;
-	int len[] = {
-		START_VALUE_FOR_INSERT,
-		START_VALUE_FOR_QUERY
-	};
-
-	for(k=0; k<2; k++){
-		initialize_base(base, RAND_RANGE);
-		shuffle_base(base, RAND_RANGE);
-
-		for(i=1; i<=3; i++){
-			print_rand(base, len[k], file_name[k][i]);
-			len[k] *= 10;
-		}
+void GetRand(int *to, int *from, int len){
+	for(int i=0; i<len; i++){
+		to[i] = from[len-1-i];
 	}
+}
+void MakeRand(int *base){
+	int output[INSERT_DATA_LEN];
+	int i;
+
+	InitializeBase(base, RAND_RANGE);
+
+		//make rand for insert
+	for(i=0; i<2; i++)
+		ShuffleBase(base, RAND_RANGE);
+	GetRand(output, base, INSERT_DATA_LEN);
+	PrintRand(output, INSERT_DATA_LEN, "./test_data/1e6_insert");
+
+		//make rand for query base on insert_data
+	for(i=0; i<2; i++)
+		ShuffleBase(output, INSERT_DATA_LEN);
+	PrintRand(output, QUERY_DATA_LEN, "./test_data/1e5_query");
 
 }
 
 
 int main(){
-	int shuffle_base[RAND_RANGE];
+	int *shuffle_base = (int*)calloc((int)1e7, sizeof(int));
 
 	system("mkdir -p test_data");
-		//making data for insert
-	make_rand(shuffle_base);
+	MakeRand(shuffle_base);
+
+	free(shuffle_base);
 
 	return 0;
 }
